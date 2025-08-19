a364efc17cb2855c2421c00d433645e8a397d595
juan <juan.ferreira3@fatec.sp.gov.br>
2025-08-19 10:19:07 -0300

    exercicios do Marcos(ainda incompletos)

---
+++ TecnicasDeProgramação1/exercicios.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+/*
+class Exercio1{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        float n1 = scanner.nextInt();
+        float n2 = scanner.nextInt();
+        System.out.println(n1 + n2);
+    }
+
+}
+*/
+
+/*
+class Exercicio2{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        float celsius = scanner.nextFloat();
+        float heit = (9 * celsius + 160)/5;
+        System.out.println(heit);
+    }
+}
+*/
+
+/*
+class Exercicio3{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        float heit = scanner.nextFloat();
+        float cel = (heit - 32) * 5/9;
+        System.out.println(cel);
+    }
+}
+*/
+
+/*
+class Exercicio4{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        double raio = scanner.nextFloat();
+        double altura = scanner.nextFloat();
+        double vol = 3.14159d * altura * Math.pow(raio, 2) ;
+        System.out.println(vol); 
+    }
+}
+*/
+
+/*
+class Exercicio5{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int dia = scanner.nextInt();
+        int mes = scanner.nextInt();
+        int ano = scanner.nextInt();
+        System.out.println(dia + (mes * 30) + ((2025 - ano) * 365));
+    }
+}
+*/
+
+/*
+class Exercicio6{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        double r = scanner.nextFloat();
+        double circu = 3.14159d * 2 * r;   
+        System.out.println((circu));     
+    }
+}
+*/
+
+class Exercicio7{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        float val = scanner.nextFloat();
+        System.out.println();     
+    }
+}
