a5b2d79bbb7584ce17bec08603b917f9ac36949f
juan <juan.ferreira3@fatec.sp.gov.br>
2025-09-02 09:16:02 -0300

    metade da 3 lista de exercicios de tecnicas de programação pronta

---
+++ TecnicasDeProgramação1/exercicios2.java
@@ -0,0 +1,28 @@
+
+/*class Exercicio1l3{
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println("Printando vetor de forma literal" + nums);
+        System.out.println("Agora vamos ver os itens do vetor");
+        for(int i = 0; i < nums.length; i++){
+            System.out.println("Item:" + nums[i]);
+        }
+    }
+}
+*/
+
+/*
+class Exercicio2l3{
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4,5,6,7,8,9,10};
+        int[] b = {a[0] * 2, a[1] * 2, a[2] * 2, a[3] * 2, a[4] * 2, a[5] * 2, a[6] * 2, a[7] * 2, a[8] * 2, a[9] * 2, };
+        System.out.println("Aqui as duas coleções");
+        for(int i = 0; i<a.length; i++){
+            System.out.println("Item:" + a[i] + " Da lista A");
+        }
+        for(int j = 0; j<b.length; j++){
+            System.out.println("Item:" + a[j] + " Da lista B");
+        }
+    }
+}
+*/
